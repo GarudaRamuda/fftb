38411d7aa33d005ef86f02314a9c395cdfcdd073
garudaramuda <jjek@ucsc.edu>
2022-10-13 12:43:23 -0700

    gameplay

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 title = "BUTTON FIGHT";
 
 description = `
+[Press] Win
 `;
 
 characters = [
   `
+  lll
+ llll
+ llll
+  lll
+  ll
+  l
+  `,
+  `
+ rrRr
+lrrRrl
+llllll
   `,
 ];
 
 options = {
 };
 
+let gameOver;
+let winThresh;
 
 function update() {
   if (!ticks) {
+    gameOver = false;
+    winThresh = rndi(300, 900);
+  }
+
+  if (input.isPressed && ticks > 30) {
+    gameOver = true;
+  }
+  if (gameOver) {
+    end();
   }
 }
 
