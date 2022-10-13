1a32da87f8fa434b61305f2c090c095edc4cf386
garudaramuda <jjek@ucsc.edu>
2022-10-13 13:15:06 -0700

    commenting

--- main.js
+++ main.js
@@ -18,6 +18,10 @@ characters = [
 lrrRrl
 llllll
   `,
+  `
+lrrRrl
+llllll
+  `
 ];
 
 options = {
@@ -25,19 +29,47 @@ options = {
 
 let gameOver;
 let winThresh;
+let fPos;
+let bDown;
 
 function update() {
   if (!ticks) {
     gameOver = false;
     winThresh = rndi(300, 900);
+    bDown = false;
   }
 
-  if (input.isPressed && ticks > 30) {
+  // play a sound every 60 or so ticks using a modulo; only play these sounds while ticks < winThresh
+  // scale the pitch of the sound up over time by using ticks as a parameter, make sure to divide ticks by a big-ish number to make it not too high pitched
+
+
+  // on the frame that ticks is exactly equal to winThresh, play a different sound once
+
+  if (input.isPressed && ticks > 60) {
     gameOver = true;
   }
+
   if (gameOver) {
-    end();
+    if (ticks > winThresh) {
+      // you win!!!! yayy :))))
+      // draw text, draw smilies; make them big, make them small, make them fly around idk
+      // and spam like particles and sounds that are kind of like airhorns
+    }
+    else {
+      // L
+      // draw big frowny :(
+      // play two really low pitched wah wah sounds or something
+    }
+    //end();
   }
+  if (!gameOver) fPos = vec(50, 50 + 10 * Math.sin(ticks / 15));
+  else {
+    fPos.y += 2;
+    fPos.y = Math.min(65, fPos.y);
+    if (fPos.y == 65) bDown = true;
+  }
+  char('a', fPos, {scale: {x: 5, y: 5}});
+  char((bDown ? "c" : "b"), 50, (bDown ? 85 : 80), {scale: {x: 5, y: 5}});
 }
 
 addEventListener("load", onLoad);
