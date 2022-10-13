f50f99c2bdf58a8e32862b51b4f98c8f26166de2
garudaramuda <jjek@ucsc.edu>
2022-10-13 11:54:02 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+title = "FIGHT FOR THE BUTTON";
+
+description = `
+`;
+
+characters = [
+  `
+  `,
+];
+
+options = {
+  isPlayingBgm: true,
+};
+
+
+function update() {
+  if (!ticks) {
+  }
+}
+
+addEventListener("load", onLoad);
